33e2444a40726459e0042610a79758b58d72231a
Javier <javiferreyra97@gmail.com>
2023-01-21 10:29:57 -0300

    Update contract to allow querying tickets owned by an address

--- infra/public_blockchain/contract/TickenEvent.sol
+++ infra/public_blockchain/contract/TickenEvent.sol
@@ -12,6 +12,12 @@ struct Ticket {
     bool    scanned;
 }
 
+struct TicketDTO {
+    uint256 id;
+    string  section;
+    bool    scanned;
+}
+
 contract TickenEvent is ERC721, Pausable, Ownable {
 
     event TicketCreated(
@@ -27,6 +33,9 @@ contract TickenEvent is ERC721, Pausable, Ownable {
     // Mapping from token ID to ticket
     mapping(uint256 => Ticket) private _tickets;
 
+    // Mapping from owner to list of owned token IDs
+    mapping(address => uint256[]) private _ownedTokens;
+
     constructor() ERC721("TickenEvent", "TE") {}
 
     function pause() public onlyOwner {
@@ -39,10 +48,28 @@ contract TickenEvent is ERC721, Pausable, Ownable {
 
     function safeMint(address to, string memory section) public whenNotPaused onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
+
         _tokenIdCounter.increment();
+
         _safeMint(to, tokenId);
+
         _tickets[tokenId] = Ticket(section, false);
+
+        _ownedTokens[to].push(tokenId);
+
         emit TicketCreated(to, tokenId, section);
+    }
+
+    // Get all tickets from an owner address
+    function getTicketsByOwner(address owner) public view returns (TicketDTO[] memory) {
+        uint256[] memory tokenIds = _ownedTokens[owner];
+        TicketDTO[] memory tickets = new TicketDTO[](tokenIds.length);
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            uint256 tokenId = tokenIds[i];
+            Ticket memory ticket = _tickets[tokenId];
+            tickets[i] = TicketDTO(tokenId, ticket.section, ticket.scanned);
+        }
+        return tickets;
     }
 
     function scanTicket(uint256 tokenId) public whenNotPaused onlyOwner {
